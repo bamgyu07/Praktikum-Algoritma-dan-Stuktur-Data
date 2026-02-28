894d4824b9b6d50e488cfa8c82b4d0efe8990ea7
bamgyu07 <alyamafaza7@gmail.com>
2026-02-28 09:33:05 +0700

    mahasiswa

---
+++ Praktikum03/Mahasiswa02.java
@@ -0,0 +1,15 @@
+package Praktikum03;
+
+public class Mahasiswa02 {
+    public String nim;
+    public String nama;
+    public String kelas;
+    public double ipk;
+
+    public void cetakInfo(){
+        System.out.println("NIM \t: " + nim);
+        System.out.println("Nama \t: " + nama);
+        System.out.println("Kelas \t: " + kelas);
+        System.out.println("IPK \t: " + ipk);
+    }
+}
