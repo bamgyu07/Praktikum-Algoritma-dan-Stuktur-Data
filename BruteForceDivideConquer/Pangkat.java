7dc61679b70fb2736222c7cdfe582a84815c46b3
bamgyu07 <alyamafaza7@gmail.com>
2026-03-15 11:14:02 +0700

    pangkat class

---
+++ BruteForceDivideConquer/Pangkat.java
@@ -0,0 +1,31 @@
+package BruteForceDivideConquer;
+
+public class Pangkat {
+    int nilai, pangkat;
+
+    Pangkat(int n, int p) {
+        this.nilai = n;
+        this.pangkat = p;
+    }
+
+    public static int pangkatBF(int a, int n) {
+        int hasil = 1;
+        for(int i = 0; i <n; i++){
+            hasil = hasil * a;
+        }
+        return hasil;
+    }
+
+    public static int pangkatDC(int a, int n) {
+        if(n == 1){
+            return a;
+        }else {
+            if(n%2 ==1){
+                return (pangkatDC(a, n/2) * pangkatDC(a, n/2) * a);
+            }
+            else {
+                return (pangkatDC(a, n/2) * pangkatDC(a, n/2));
+        }
+        }
+    }
+}
