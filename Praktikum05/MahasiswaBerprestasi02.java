1dc45f1f5c1def9f232af5143ee9f5d3f98f4f84
bamgyu07 <alyamafaza7@gmail.com>
2026-04-07 17:34:44 +0700

    mahasiswa berprestasi (selection sort)

--- Praktikum05/MahasiswaBerprestasi02.java
+++ Praktikum05/MahasiswaBerprestasi02.java
@@ -35,4 +35,20 @@ public class MahasiswaBerprestasi02 {
             }
         }
     }
+
+    void selectionSort(){
+        for (int i = 0; i < listMhs.length-1; i++) {
+            int idxMin=i;
+            for (int j = i + 1; j <listMhs.length; j++) {
+                if (listMhs[j] != null && listMhs[idxMin] != null) {
+                    if (listMhs[j].ipk < listMhs[idxMin].ipk) {
+                        idxMin = j;
+                    }
+                }
+            }
+            Mahasiswa02 tmp = listMhs[idxMin];
+            listMhs[idxMin] = listMhs[i];
+            listMhs[i] = tmp;
+        }
+    }
 }
