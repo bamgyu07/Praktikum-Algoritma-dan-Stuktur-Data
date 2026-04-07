b3f8528fbfbb2a03e174a13021358cf8d1e8047b
bamgyu07 <alyamafaza7@gmail.com>
2026-04-07 17:29:11 +0700

    mahasiswa berprestasi

---
+++ Praktikum05/MahasiswaBerprestasi02.java
@@ -0,0 +1,38 @@
+package Praktikum05;
+
+public class MahasiswaBerprestasi02 {
+    Mahasiswa02[] listMhs = new Mahasiswa02[5];
+    int idx;
+
+    void tambah(Mahasiswa02 m) {
+        if (idx < listMhs.length) {
+            listMhs[idx] = m;
+            idx++;
+        } else {
+            System.out.println("Data sudah penuh!!");
+        }
+    }
+
+    void tampil() {
+        for (Mahasiswa02 m : listMhs) {
+            if (m != null) {
+                m.tampilInformasi();
+                System.out.println("--------------------");
+            }
+        }
+    }
+
+    void bubbleSort() {
+        for (int i = 0; i < listMhs.length - 1; i++) {
+            for (int j = 1; j < listMhs.length - i; j++) {
+                if (listMhs[j] != null && listMhs[j - 1] != null) {
+                    if (listMhs[j].ipk > listMhs[j - 1].ipk) {
+                        Mahasiswa02 tmp = listMhs[j];
+                        listMhs[j] = listMhs[j - 1];
+                        listMhs[j - 1] = tmp;
+                    }
+                }
+            }
+        }
+    }
+}
