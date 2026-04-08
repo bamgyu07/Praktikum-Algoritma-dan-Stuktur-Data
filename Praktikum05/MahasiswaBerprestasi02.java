adf50e6471e2e1cf3dfa38c85e36c2b6d2102516
bamgyu07 <alyamafaza7@gmail.com>
2026-04-08 11:15:01 +0700

    revv

--- Praktikum05/MahasiswaBerprestasi02.java
+++ Praktikum05/MahasiswaBerprestasi02.java
@@ -1,7 +1,7 @@
 package Praktikum05;
 
 public class MahasiswaBerprestasi02 {
-    Mahasiswa02[] listMhs = new Mahasiswa02[5];
+    Mahasiswa02[] listMhs;
     int idx;
 
     void tambah(Mahasiswa02 m) {
@@ -61,6 +61,51 @@ public class MahasiswaBerprestasi02 {
                 j--;
             }
             listMhs[j + 1] = temp;
+        }
+    }
+
+    int sequentialSearching(double cari){
+        int posisi = -1;
+        for (int j = 0; j < listMhs.length; j++){
+            if (listMhs[j].ipk == cari) {
+                posisi = j;
+                break;
+            }
+        }
+        return posisi;
+    }
+
+    int findBinarySearch(double cari, int left, int right){
+        int mid;
+        if (right >= left){
+            mid = (left + right) / 2;
+            if (cari == listMhs[mid].ipk){
+                return mid;
+            } else if (listMhs[mid].ipk < cari){
+                return findBinarySearch(cari, left, mid - 1);
+            } else {
+                return findBinarySearch(cari, mid + 1, right);
+            }
+        }
+        return -1;
+    }
+
+    void tampilPosisi(double x, int pos){
+        if (pos!=-1){
+            System.out.println("Data mahasiswa dengan IPK " + x + " ditemukan pada indeks " + pos);
+        } else {
+            System.out.println("Data " + x + " tidak ditemukan");
+        }
+    }
+
+    void tampilDataSearch(double x, int pos){
+        if (pos != -1){
+            System.out.println("nim\t : "+ listMhs[pos].nim);
+            System.out.println("nama\t : "+ listMhs[pos].nama);
+            System.out.println("kelas\t : "+ listMhs[pos].kelas);
+            System.out.println("ipk\t : "+ x);
+        } else {
+            System.out.println("Data mahasiswa dengan IPK " + x + " tidak ditemukan");
         }
     }
 }
