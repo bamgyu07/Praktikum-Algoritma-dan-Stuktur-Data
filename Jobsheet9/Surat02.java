0331a9dff8f2248fa7a7f2bc69d7957226c1392d
bamgyu07 <alyamafaza7@gmail.com>
2026-04-27 08:13:34 +0700

    surat class

---
+++ Jobsheet9/Surat02.java
@@ -0,0 +1,20 @@
+package Jobsheet9;
+
+public class Surat02 {
+    String idSurat;
+    String namaMahasiswa;
+    String kelas;
+    char jenisIzin;
+    int durasi;
+
+    Surat02(){
+    }
+
+    Surat02(String idSurat, String namaMahasiswa, String kelas, char jenisIzin, int durasi) {
+        this.idSurat = idSurat;
+        this.namaMahasiswa = namaMahasiswa;
+        this.kelas = kelas;
+        this.jenisIzin = jenisIzin;
+        this.durasi = durasi;
+    }
+}
